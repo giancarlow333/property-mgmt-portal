fe678a7bb1e6c7c590e29b66b3a45361e0b21115
Giancarlo Whitaker <giancarlo.whitaker@gmail.com>
2023-07-26 20:43:45 -0700

    Rename files; finalize highlight script(s) to highlight current page in navbar

--- assets/script1.js
+++ assets/script1.js
@@ -1,4 +1,16 @@
-if (currentPage === "home") {
-    var menuElt = document.getElementById("home-elt");
+if (currentPage === "reimbursement") {
+    var menuElt = document.getElementById("reimbursement");
     menuElt.setAttribute("style", "background-color: blue;");
+}
+else if (currentPage === "search") {
+    var menuElt = document.getElementById("search");
+    menuElt.setAttribute("style", "background-color: blue;");
+}
+else if (currentPage === "rules") {
+    var menuElt = document.getElementById("rules");
+    menuElt.setAttribute("style", "background-color: blue;");
+}
+else if (currentPage === "properties") {
+    var menuElt = document.getElementById("properties");
+    menuElt.setAttribute("class", "bg-cyan-700");
 };
