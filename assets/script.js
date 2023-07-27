a37c6056df95774fb9cf7bf0cd672216f026e76e
Giancarlo Whitaker <giancarlo.whitaker@gmail.com>
2023-07-26 20:20:40 -0700

    Attempt to use JavaScript to access <body> elt

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,9 @@
-console.log(document.location);
+//var bodyElt = document.getElementsByTagName("body")[0];
+var bodyElt = document.body;
+console.log(bodyElt);
+console.log(document.body);
+var currentPage = bodyElt.getAttribute("data-page");
+if (currentPage === "home") {
+    var menuElt = document.getElementById("home");
+    menuElt.setAttribute("style", "background-color: blue;");
+};
