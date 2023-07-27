9fc23fd2517cb604cd9cd1cbd7a0ab9c2413619f
Giancarlo Whitaker <giancarlo.whitaker@gmail.com>
2023-07-26 20:45:37 -0700

    Save script1.js

--- assets/script1.js
+++ assets/script1.js
@@ -1,14 +1,14 @@
 if (currentPage === "reimbursement") {
     var menuElt = document.getElementById("reimbursement");
-    menuElt.setAttribute("style", "background-color: blue;");
+    menuElt.setAttribute("class", "bg-cyan-700");
 }
 else if (currentPage === "search") {
     var menuElt = document.getElementById("search");
-    menuElt.setAttribute("style", "background-color: blue;");
+    menuElt.setAttribute("class", "bg-cyan-700");
 }
 else if (currentPage === "rules") {
     var menuElt = document.getElementById("rules");
-    menuElt.setAttribute("style", "background-color: blue;");
+    menuElt.setAttribute("class", "bg-cyan-700");
 }
 else if (currentPage === "properties") {
     var menuElt = document.getElementById("properties");
