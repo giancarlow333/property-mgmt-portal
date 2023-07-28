e9a02adf6ae9dd2656391c34544958663802fdc0
Giancarlo Whitaker <giancarlo.whitaker@gmail.com>
2023-07-27 21:09:18 -0700

    Get it to search Google API with JS (instead of Yelp)

--- assets/script2.js
+++ assets/script2.js
@@ -1,22 +1,25 @@
 /* YELP API SCRIPT */
 
-const APIKey = "TD7vrnDl1KyIr9EMYMgxF0dc7ChZPzZOsRLIDJqnej4Iclbtgxv7coJzTOpVv2jJXojL6nkjHyXQAXeuN__P2qlrtmh2uDadaFMxeea5OmEa3HTV4_FFRwLkhirDZHYx";
-const baseURL = "https://api.yelp.com/v3/businesses/search?";
+const APIKey = "AIzaSyBpzAJtDlPPG6C2l0fUZM558KSRGdIRbT8";
+const baseURL = "https://customsearch.googleapis.com/customsearch/v1?";
+const searchEngineID = "54f51fe0be357423e";
 
 var searchLocation = "350%205th%20Ave,%20New%20York,%20NY%2010118";
 var searchTerm = "plumber";
 
-var requestURL = baseURL + "location=" + searchLocation + "&categories=" + searchTerm;
+var requestURL = baseURL + "key=" + APIKey + "&cx=" + searchEngineID + "&q=\"" + searchTerm + "%20AND%20" + searchLocation + "\"";
 
-const options = {
-    headers: {
-      Authorization: "Bearer " + APIKey,
-      accept: "application/json"
-    }
-  };
 
 // https://stackoverflow.com/questions/25515936/perform-curl-request-in-javascript
 // by user https://stackoverflow.com/users/1244127/json-c11
-fetch(requestURL, options)
+fetch(requestURL)
   .then(response => response.json())
   .then(data => console.log(data));
+
+  /*
+  <script async src="https://cse.google.com/cse.js?cx=54f51fe0be357423e">
+</script>
+<div class="gcse-search"></div>
+*/
+
+// https://developers.google.com/custom-search/v1/using_rest
