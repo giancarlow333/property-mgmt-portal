77c641c4329efa273bacc7ea4ef0b08c970c0ad8
Giancarlo Whitaker <giancarlo.whitaker@gmail.com>
2023-07-31 18:49:46 -0700

    Add code following Google's documentation

--- assets/script2.js
+++ assets/script2.js
@@ -1,4 +1,4 @@
-/* YELP API SCRIPT */
+/* GOOGLE API SCRIPT */
 
 const APIKey = "AIzaSyBpzAJtDlPPG6C2l0fUZM558KSRGdIRbT8";
 const baseURL = "https://customsearch.googleapis.com/customsearch/v1?";
@@ -13,9 +13,11 @@ var requestURL = baseURL + "key=" + APIKey + "&cx=" + searchEngineID + "&q=\"" +
 // https://stackoverflow.com/questions/25515936/perform-curl-request-in-javascript
 // by user https://stackoverflow.com/users/1244127/json-c11
 fetch(requestURL)
-  .then(response => response.json())
-  .then(data => console.log(data));
-
+  .then(function (response) {
+    response.json().then(function(data) {
+      console.log(data);
+    });
+  });
   /*
   <script async src="https://cse.google.com/cse.js?cx=54f51fe0be357423e">
 </script>
@@ -23,3 +25,12 @@ fetch(requestURL)
 */
 
 // https://developers.google.com/custom-search/v1/using_rest
+function displayResults(response) {
+  for (var i = 0; i < 2; i++) {
+    var item = response.items[i];
+    document.getElementById("search-results").append(
+      document.createElement("br"),
+      document.createTextNode(item.htmlTitle)
+    );
+  }
+}
