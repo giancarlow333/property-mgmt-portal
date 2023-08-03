521fe531ed60e15fd753ab4af4a927c0d163b326
alathea23 <135459921+alathea23@users.noreply.github.com>
2023-08-02 18:23:08 -0700

    update api

--- assets/script1.js
+++ assets/script1.js
@@ -1,4 +1,59 @@
 
+const APIKey = "md-NbUjRtqyFGcJ8YTmbRAu7A";
+const baseURL = "https://customsearch.googleapis.com/customsearch/v1?";
+const searchEngineID = "54f51fe0be357423e";
+var searchLocation;
+var searchTerm;
+var requestURL;
+
+# Run:
+curl -s \
+-X POST \
+--user "$MJ_APIKEY_PUBLIC:$MJ_APIKEY_PRIVATE" \
+https://api.mailjet.com/v3.1/send \
+-H 'Content-Type: application/json' \
+-d '{
+
+
+    curl -sS -X POST "https://mandrillapp.com/api/1.0/users/ping" \
+    --header 'Content-Type: application/json' \
+    --data-raw '{ "key": "YOUR_API_KEY" }'
+
+
+    const mailchimpClient = require("@mailchimp/mailchimp_transactional")(
+        "md-NbUjRtqyFGcJ8YTmbRAu7A"
+      );
+      
+      const run = async () => {
+        const response = await mailchimpClient.messages.send({ message: {} });
+        console.log(response);
+      };
+      
+      run();
+      
+}
+]
+}'
+
+=================================
+
+var options = {
+  hostname: 'posttestserver.com',
+  port: 443,
+  path: '/post.php',
+  method: 'POST',
+  headers: {
+       'Content-Type': 'application/x-www-form-urlencoded',
+       'Content-Length': postData.length
+     }
+};
+
+
+
+
+
+// JS email send option
+
 var submitB = getElementbyID("button")
 
 Email.send({
@@ -15,16 +70,16 @@ Email.send({
 
 addEventListener.submitB("click", Email.send)
 
-const APIKey = "";
-const baseURL = "https://customsearch.googleapis.com/customsearch/v1?";
-const senderURL = "https://api.mailjet.com/v3/REST/sender"
+//const APIKey = "";
+//const baseURL = "https://customsearch.googleapis.com/customsearch/v1?";
+//const senderURL = "https://api.mailjet.com/v3/REST/sender"
 
-string userAgent = "mailjet-apiv3-java/v5.2.3"
+//string userAgent = "mailjet-apiv3-java/v5.2.3"
 
-var searchLocation = "350%205th%20Ave,%20New%20York,%20NY%2010118";
-var searchTerm = "plumber";
+//var searchLocation = "350%205th%20Ave,%20New%20York,%20NY%2010118";
+//var searchTerm = "plumber";
 
-var requestURL = baseURL + "key=" + APIKey + "&cx=" + searchEngineID + "&q=\"" + searchTerm + "%20AND%20" + searchLocation + "\"";
+//var requestURL = baseURL + "key=" + APIKey + "&cx=" + searchEngineID + "&q=\"" + searchTerm + "%20AND%20" + searchLocation + "\"";
 
 
 // https://stackoverflow.com/questions/25515936/perform-curl-request-in-javascript
