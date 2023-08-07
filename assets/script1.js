a3af57289bdd4ab207564467d02a6b9b180f71ec
alathea23 <135459921+alathea23@users.noreply.github.com>
2023-08-06 23:05:14 -0700

    title color and alignments

--- assets/script1.js
+++ assets/script1.js
@@ -6,12 +6,12 @@ var apiMC = "md-NbUjRtqyFGcJ8YTmbRAu7A";
 var baseURL = "https://customsearch.googleapis.com/customsearch/v1?";
 
 
-var responseLog = document.querySelector("#formsReturn").value 
+var responseLog = document.querySelector("#formsReturn").value
 responseLog = "We respond to submissions within 6 hours"
 
 
 //function SendGrid () {
-  //var Key = "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"
+//var Key = "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"
 //var Server = "smtp.sendgrid.net"
 //var Ports = [25, 587]
 //UserName = "apikey"
@@ -20,43 +20,45 @@ responseLog = "We respond to submissions within 6 hours"
 
 
 //testing API
-function TestAPI () {
+function TestAPI() {
   fetch("https://mandrillapp.com/api/1.0/users/ping", {
-  method: "POST",
-  body: JSON.stringify({
-  key: "md-NbUjRtqyFGcJ8YTmbRAu7A"
-  }),
-  headers: {
-  "Content-Type": "application/json"
-  }
-  })
-  .then((response) => response.json())
-  .then((json) => console.log(json));
-  }
-
-  TestAPI ()
-  console.log("tested")
-
-   //send first transactional
-   function Email () {
-    fetch("https://mandrillapp.com/api/1.0/messages/send", {
-        method: "POST",
-        body: JSON.stringify({
-            key: "md-NbUjRtqyFGcJ8YTmbRAu7A", message: 
-            {from_email: "jennywnba@gmail.com", subject: "Hello World", 
-            text:"Sending our first email:", to: 
-            [{ email: "jennywnba@gmail.com", type: "to"}]}
-        }),
-        headers: {
-          "Content-Type": "application/json"
-        }
-      })
-      .then((response) => response.json())
-      .then((json) => console.log(json))
+    method: "POST",
+    body: JSON.stringify({
+      key: "md-NbUjRtqyFGcJ8YTmbRAu7A"
+    }),
+    headers: {
+      "Content-Type": "application/json"
     }
+  })
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+}
 
-      // to add into text once a text email sends:
-      //"name:+ inputName + "  phone: " + inputPhone + "   email: " + inputEmail + "  issue-type: " + inputIssueType + "  description:" + inputDescription
+TestAPI()
+console.log("tested")
+
+//send first transactional
+function Email() {
+  fetch("https://mandrillapp.com/api/1.0/messages/send", {
+    method: "POST",
+    body: JSON.stringify({
+      key: "md-NbUjRtqyFGcJ8YTmbRAu7A", message:
+      {
+        from_email: "jennywnba@gmail.com", subject: "Hello World",
+        text: "Sending our first email:", to:
+          [{ email: "jennywnba@gmail.com", type: "to" }]
+      }
+    }),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  })
+    .then((response) => response.json())
+    .then((json) => console.log(json))
+}
+
+// to add into text once a text email sends:
+//"name:+ inputName + "  phone: " + inputPhone + "   email: " + inputEmail + "  issue-type: " + inputIssueType + "  description:" + inputDescription
 
 // Run script when button is clicked
 $(function () {
@@ -64,26 +66,25 @@ $(function () {
   var btnElt = $('#button');
 
   // add listener to button
-  btnElt.on("click", function(event) {
+  btnElt.on("click", function (event) {
     event.preventDefault();
 
     Email()
 
     var inputEmail = $("#email").val();
 
-var inputName= $("#username").val();
+    var inputName = $("#username").val();
 
-var inputPhone= $("#phone").val();
+    var inputPhone = $("#phone").val();
 
-var inputProperty= $("#property").val();
-localStorage.setItem("property", inputProperty.value);
+    var inputProperty = $("#property").val();
+    localStorage.setItem("property", inputProperty.value);
 
-var inputDescription= $("#description").val();
-localStorage.setItem("description", inputDescription.value);
+    var inputDescription = $("#description").val();
+    localStorage.setItem("description", inputDescription.value);
 
     // set issue type based on selection
     var issueType = $("#issue-type").find(":selected").val();
-
 
     if (issueType == "Reimbursement") {
       emailSubject = "Reimbursement Request";
@@ -95,22 +96,42 @@ localStorage.setItem("description", inputDescription.value);
       emailSubject = "Electrical Service Request";
     }
     else if (issueType == "Complaint") {
-      searchLocation = "Tenant Complaint";}
+      searchLocation = "Tenant Complaint";
+    }
 
     else if (issueType == "Other Issue") {
-        emailSubject = "Tenant Issue"; };
+      emailSubject = "Tenant Issue";
+    };
 
-        document.querySelector("#formsReturn").value = "(Email Sent) "+ " name:" + inputName + "  phone: " + inputPhone + "   email: " + inputEmail + "  issue-type: " + issueType + "  description:  " + inputDescription;
+    var propertyID = $("#property").find(":selected").val();
+    // set property type based on selection
+    if (propertyID == "The Custom - Luxury Suites") {
+      searchLocation = "90247";
+    }
+    else if (propertyID == "JW Lofts") {
+      searchLocation = "90247";
+    }
+    else if (propertyID == "The Giancarlo") {
+      searchLocation = "90042";
+    }
+    else if (propertyID == "Crystal Lake Apartments") {
+      searchLocation = "90066";
+    }
+    var emergency = $("#eYes").val()
 
-        console.log("button pressed")
-        console.log(emailSubject)
-        console.log(inputEmail)
-    })
-    console.log("button listening")
+    if (emergency)
+
+      document.querySelector("#formsReturn").value = "(Email Sent) " + " name:" + inputName + "  phone: " + inputPhone + "   email: " + inputEmail + "  issue-type: " + issueType + "  description:  " + inputDescription;
+
+    console.log("button pressed")
+    console.log(emailSubject)
+    console.log(inputEmail)
   })
+  console.log("button listening")
+})
 
 
-  const form = document.querySelector('form');
+const form = document.querySelector('form');
 
 function logForm(event) {
   event.preventDefault();
